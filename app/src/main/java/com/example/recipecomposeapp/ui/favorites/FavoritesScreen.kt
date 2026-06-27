b1cf5dc1d07b80d77eff79b1cea79a7353c408ac
Alexander <a_36524@mail.ru>
2026-06-27 07:22:28 +0400

    RCA-10 FavoritesScreen-RecipesScreen

---
+++ app/src/main/java/com/example/recipecomposeapp/ui/favorites/FavoritesScreen.kt
@@ -0,0 +1,32 @@
+package com.example.recipecomposeapp.ui.favorites
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.recipecomposeapp.R
+import com.example.recipecomposeapp.ui.components.ScreenHeader
+import com.example.recipecomposeapp.ui.theme.Dimens
+
+@Composable
+fun FavoritesScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column {
+            ScreenHeader(
+                imagePainter = painterResource(R.drawable.bcg_favorites),
+                contentDescription = "Фоновая фотография еды",
+                title = "ИЗБРАННОЕ",
+            )
+            Text(
+                text = "Заглушка: Список избранных рецептов",
+                modifier = Modifier.padding(Dimens.paddingMain)
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recipecomposeapp/ui/recipes/RecipesScreen.kt
@@ -0,0 +1,32 @@
+package com.example.recipecomposeapp.ui.recipes
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.recipecomposeapp.R
+import com.example.recipecomposeapp.ui.components.ScreenHeader
+import com.example.recipecomposeapp.ui.theme.Dimens
+
+@Composable
+fun RecipesScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column {
+            ScreenHeader(
+                imagePainter = painterResource(R.drawable.bcg_recipes_list),
+                contentDescription = "Фоновая фотография еды",
+                title = "РЕЦЕПТЫ",
+            )
+            Text(
+                text = "Скоро здесь будет список рецептов",
+                modifier = Modifier.padding(Dimens.paddingMain)
+            )
+        }
+    }
+}
