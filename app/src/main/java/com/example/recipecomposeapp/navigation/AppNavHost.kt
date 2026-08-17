a177374c88b61fd7b845dfe1f028cee0d60b9dc0
Alexander <a_36524@mail.ru>
2026-08-17 06:36:38 +0400

    RCA-19 Deep Links

--- app/src/main/java/com/example/recipecomposeapp/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/recipecomposeapp/navigation/AppNavHost.kt
@@ -1,6 +1,8 @@
 package com.example.recipecomposeapp.navigation
 
+import android.content.Intent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -12,11 +14,32 @@ import com.example.recipecomposeapp.ui.error.ErrorScreen
 import com.example.recipecomposeapp.ui.favorites.FavoritesScreen
 import com.example.recipecomposeapp.ui.recipes.RecipesScreen
 import com.example.recipecomposeapp.ui.recipes.model.RecipeUiModel
+import kotlinx.coroutines.delay
 
 @Composable
 fun AppNavHost(
     navController: NavHostController,
+    deepLinkIntent: Intent?,
 ) {
+    LaunchedEffect(deepLinkIntent) {
+        deepLinkIntent?.data?.let { uri ->
+            val recipeId: Int? = when (uri.scheme) {
+                "recipeapp" ->
+                    // recipeapp://recipe/123 → host="recipe", pathSegments=[123]
+                    if (uri.host == "recipe") uri.pathSegments[0].toIntOrNull() else null
+                "https", "http" ->
+                    // https://.../recipe/123 → pathSegments=[recipe, 123]
+                    if (uri.pathSegments[0] == "recipe") uri.pathSegments[1].toIntOrNull() else null
+                else -> null
+            }
+
+            if (recipeId != null) {
+                delay(100) // даем время на инициализацию графа
+                navController.navigate(Destination.Recipe.createRoute(recipeId))
+            }
+        }
+    }
+
     NavHost(
         navController = navController,
         startDestination = Destination.Categories.route,
--- app/src/main/java/com/example/recipecomposeapp/navigation/Destination.kt
+++ app/src/main/java/com/example/recipecomposeapp/navigation/Destination.kt
@@ -9,9 +9,16 @@ sealed class Destination(val route: String) {
         fun createRoute(categoryId: Int, categoryTitle: String) =
             "recipes/$categoryId/${Uri.encode(categoryTitle)}"
     }
-    object Recipe : Destination("recipe/{recipeId}") {
+//    object Recipe : Destination("recipe/{recipeId}") {
+    object Recipe : Destination("recipe/{$PARAM_RECIPE_ID}") {
         fun createRoute(recipeId: Int) =
             "recipe/$recipeId"
     }
     object Favorites : Destination("favorites")
 }
+
+const val DEEP_LINK_SCHEME = "recipeapp"
+const val DEEP_LINK_BASE_URL = "https://recipes.androidsprint.ru"
+
+fun createRecipeDeepLink(recipeId: Int) =
+    "$DEEP_LINK_BASE_URL/recipe/$recipeId"
